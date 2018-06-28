86d387255213d76f0f103ad7a038bcd5d77deb98
Michael Ballard <ballard.michael9@gmail.com>
2018-06-28 14:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,12 @@ function BoardMember(name, homeState, training){
     return 'You can do that!'
   }
   
+  this.doCharity = function(){
+    return 'I like to help people.'
+  }
+  
+  this.releasePressStatement = function(){
+    return 'You will see great things from Scuber'
+  }
+  
 }
