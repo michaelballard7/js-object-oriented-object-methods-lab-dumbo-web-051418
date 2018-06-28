87232a0484aade1553d3333bf558d13c7e31c47e
Michael Ballard <ballard.michael9@gmail.com>
2018-06-28 14:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,4 +22,6 @@ function BoardMember(name, homeState, training){
   this.sayHi = function(){
     
     let message = `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    return message
+    
 }
